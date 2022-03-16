98a671a06ba89a768c68459c92ff4631418bd665
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-16 23:22:01 +0530

    'del'

--- 14th March/dsa.js
+++ 14th March/dsa.js
@@ -1,9 +1,25 @@
-let n = 9;
-
-function print(n) {
-  if (n == 0) return;
-  console.log(n);
-  print(n - 1);
+function runProgram(input=''){
+  
+    
 }
 
-print(n);
+if (process.env.USER === "") {
+  runProgram(`1 2 3 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
